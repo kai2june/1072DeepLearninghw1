c17748ee1941f355f15d8be0831e5bf57204b9d6
kai2june <104703027@nccu.edu.tw>
2019-04-25 14:02:10 +0800

    hw1q5.py comments that helps to understand is added.

--- hw1q5.py
+++ hw1q5.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 
 train = scipy.io.loadmat('train.mat')
 test = scipy.io.loadmat('test.mat')
+print(len(train['x1']))
 X_train = np.concatenate((train['x1'], train['x2']), axis=1)
 X_test = np.concatenate((test['x1'], test['x2']), axis=1)
 
@@ -15,11 +16,18 @@ logreg.fit(X_train, train['y'])
 x_min, x_max = train['x1'][:, 0].min() - 0.5, train['x1'][:, 0].max() + 0.5
 y_min, y_max = train['x2'][:, 0].min() - 0.5, train['x2'][:, 0].max() + 0.5
 h = 0.001
+
+# determine the area x axis range, yaxis range to draw
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+print(xx, yy)
+
+#  predict the area (xmin~ xmax ,  ymin~ymax) 's  background  color  by  'logreg' classifier, and then assign to Z
 Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 plt.figure(1, figsize=(4,3))
 plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+# predict for test data
 predict_testY = logreg.predict(X_test)
 
 # count accuracy
